ecf8ce65c09c36257c0bb39218da5a139fa91204
laverka <valeriy.tovstyk@gmail.com>
2020-02-19 03:59:24 +0200

    update method

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,6 +1,6 @@
 const Message = require('../models/Message');
 const mongoose = require('mongoose');
-const limit = 3;
+const limit = 10;
 
 const MessageController = {
     getMessagesList: async (ctx) => {
@@ -22,35 +22,32 @@ const MessageController = {
     },
     getById: async (ctx) => {
         if (mongoose.Types.ObjectId.isValid(ctx.params.message_id)) {
-            Message.findOne({_id: ctx.params.message_id})
-                .then((doc) => {
-                    if (doc) {
-                        console.log(doc);
-                    } else {
-                        console.log("no data exist for this id");
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+            try {
+                ctx.body = await Message.findOne({_id: ctx.params.message_id})
+                    .skip(ctx.params.page * limit)
+                    .limit(limit)
+            } catch (err) {
+                ctx.status = 204;
+            }
         } else {
             console.log("please provide correct id");
         }
     },
+
     addMessage: async (ctx) => {
-        console.log("add new user");
-        Message.collection.insertOne(
-            {
-                message_text: ctx.request.body.message_text,
-                author_name: ctx.request.body.author_name,
-                author_email: ctx.request.body.author_email
-            }
-        )
-            .then((data) => {
-                console.log(data);
-            }).catch((err) => {
-            console.log(err);
-        })
+        try {
+            const test = await Message.collection.insertOne(
+                {
+                    message_text: ctx.request.body.message_text,
+                    author_name: ctx.request.body.author_name,
+                    author_email: ctx.request.body.author_email
+                }
+
+            );
+
+        } catch (err) {
+            ctx.status = 204;
+        }
     }
 };
 
